f24aa4a1d68845f7fbd8b2520b83e4c842caa349
Hirohi Maruyama <maruyama@near.co.jp>
2016-05-26 22:01:27 +0900

    a

--- python/demo/main.py
+++ python/demo/main.py
@@ -1,3 +1,4 @@
+# coding: utf-8
 # Copyright 2012 Google Inc. All Rights Reserved.
 
 #[START sample]
@@ -26,9 +27,14 @@ class MainPage(webapp2.RequestHandler):
 
 #[START get_default_bucket]
   def get(self):
+
+    # デフォルトのバケット名を取得する
     bucket_name = os.environ.get('BUCKET_NAME',
                                  app_identity.get_default_gcs_bucket_name())
 
+    # yellow-1317.appspot.com
+    logging.debug(bucket_name)
+    # bucket_name = "y-bucket1"
     self.response.headers['Content-Type'] = 'text/plain'
     self.response.write('Demo GCS Application running from Version: '
                         + os.environ['CURRENT_VERSION_ID'] + '\n')
@@ -40,6 +46,7 @@ class MainPage(webapp2.RequestHandler):
     self.tmp_filenames_to_clean_up = []
 
     try:
+      # Cloud Strage へファイルを書き込む
       self.create_file(filename)
       self.response.write('\n\n')
 
@@ -81,22 +88,26 @@ class MainPage(webapp2.RequestHandler):
     self.response.write('Creating file %s\n' % filename)
 
     write_retry_params = gcs.RetryParams(backoff_factor=1.1)
+    # 書き込みを行うためにファイルをオープンする。カスタムメタデータを書いた Cloud Strage Headerを指定する
     gcs_file = gcs.open(filename,
                         'w',
                         content_type='text/plain',
                         options={'x-goog-meta-foo': 'foo',
                                  'x-goog-meta-bar': 'bar'},
                         retry_params=write_retry_params)
+    # 書き込む
     gcs_file.write('abcde\n')
     gcs_file.write('f'*1024*4 + '\n')
+    # 書き終わったのファイルを閉じる。これを忘れるとCloud Strageへファイルが書き込まれない。
     gcs_file.close()
     self.tmp_filenames_to_clean_up.append(filename)
 #[END write]
 
 #[START read]
   def read_file(self, filename):
+    """ バケットからファイルを読み込む """
     self.response.write('Abbreviated file content (first line and last 1K):\n')
-
+    # ファイルを開く。モード指定がないので r モード。
     gcs_file = gcs.open(filename)
     self.response.write(gcs_file.readline())
     gcs_file.seek(-1024, os.SEEK_END)
@@ -109,6 +120,7 @@ class MainPage(webapp2.RequestHandler):
 
     stat = gcs.stat(filename)
     self.response.write(repr(stat))
+    # (filename: /yellow-1317.appspot.com/demo-testfile, st_size: 4103, st_ctime: 1464260600.0, etag: 9eb45a6c9ae026180b111a900c742bf8, content_type: text/plain, metadata: {'x-goog-meta-bar': 'bar', 'x-goog-meta-foo': 'foo', 'cache-control': 'private, max-age=0'})
 
   def create_files_for_list_bucket(self, bucket):
     self.response.write('Creating more files for listbucket...\n')
@@ -125,6 +137,8 @@ class MainPage(webapp2.RequestHandler):
 
     Args:
       bucket: bucket.
+    """
+    """ バケットが大量にあった時にページング表示させる方法
     """
     self.response.write('Listbucket result:\n')
 
@@ -144,6 +158,8 @@ class MainPage(webapp2.RequestHandler):
 #[END list_bucket]
 
   def list_bucket_directory_mode(self, bucket):
+    """ ディレクトリとファイル両方とも出力する
+    """
     self.response.write('Listbucket directory mode result:\n')
     for stat in gcs.listbucket(bucket + '/b', delimiter='/'):
       self.response.write('%r' % stat)
